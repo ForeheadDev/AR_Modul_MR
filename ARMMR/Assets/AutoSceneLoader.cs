d831788f10dd42019e25319e58bb4c76c8b48bca
ForeheadDev <foreheaddev@gmail.com>
2020-02-05 14:37:15 +0700

    Add Transition with Scene Loader System

---
+++ ARMMR/Assets/AutoSceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AutoSceneLoader : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadMyScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        
+    }
+
+    IEnumerator LoadMyScene(int levelIndex)
+    {
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ ARMMR/Assets/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadMyScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    public void LoadPreviousScene()
+    {
+        StartCoroutine(LoadMyScene(SceneManager.GetActiveScene().buildIndex -1));
+    }
+
+    IEnumerator LoadMyScene(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
